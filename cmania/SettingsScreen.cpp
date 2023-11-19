7c0e5467f670a10b523868554a22975bc42e54b6
telecomadm1145 <1587496147@qq.com>
2023-11-19 16:36:49 +0800

    新增键位绑定界面

--- cmania/SettingsScreen.cpp
+++ cmania/SettingsScreen.cpp
@@ -4,6 +4,82 @@
 #include <vector>
 #include "SettingsScreen.h"
 class SettingsScreen : public Screen {
+	class KeyBindingSetupScreen : public Screen {
+	private:
+		int i = 0;
+		virtual void Render(GameBuffer& buf) {
+			auto set = game->Settings["KeyBinds"];
+			std::string prompt;
+			if (i >= 18) {
+				prompt = "已完成,Esc返回";
+			}
+			else {
+				prompt = "请输入你欲设置的键,Esc返回并保存,Tab复原(现在是第";
+				prompt += std::to_string(i);
+				prompt += "个键)\n";
+				auto t = set.GetArray<ConsoleKey>();
+				int p = 0;
+				int d = 0;
+				for (auto j = t; j < t + 18; j++) {
+					auto text = std::to_string((int)*j);
+					if ((int)*j >= 32 && (int)*j <= 'Z') {
+						text = '(';
+						text += (char)*j;
+						text += ')';
+					}
+					prompt += text;
+					prompt += ' ';
+					if (j - t == i)
+						d = p;
+					p += text.size() + 1;
+				}
+				prompt += '\n';
+				prompt.resize(prompt.size() + d, ' ');
+				prompt += '^';
+			}
+			buf.DrawString(prompt, 0, 0, {}, {});
+		}
+		virtual void Key(KeyEventArgs kea) {
+			auto t = game->Settings["KeyBinds"].GetArray<ConsoleKey>();
+			if (kea.Pressed) {
+				if (kea.Key == ConsoleKey::Escape) {
+					game->Settings.Write();
+					parent->Back();
+					return;
+				}
+				if (kea.Key == ConsoleKey::Tab) {
+					game->Settings.Read();
+					return;
+				}
+				if (kea.Key == ConsoleKey::Backspace) {
+					t[i] = (ConsoleKey)'?';
+					return;
+				}
+				if (kea.Key == ConsoleKey::LeftArrow) {
+					if (i > 0)
+						i--;
+					return;
+				}
+				if (kea.Key == ConsoleKey::RightArrow) {
+					if (i < 17)
+						i++;
+					return;
+				}
+				if (i < 17)
+					if (kea.UnicodeChar >= 32) {
+						auto repeating = false;
+						for (auto o = t; o < t + 18; o++) {
+							if (*o == kea.Key && o - t != i)
+								repeating = true;
+						}
+						if (!repeating) {
+							t[i] = kea.Key;
+							i++;
+						}
+					}
+			}
+		}
+	};
 	int scroll = 0;
 	virtual void Render(GameBuffer& buf) {
 		std::string line = "选项(用上下箭头翻页):\n";
@@ -36,6 +112,7 @@ class SettingsScreen : public Screen {
 		line.append("ms(+/-调整,Shift==0.1,大写锁定==10,判定偏移量(不是歌曲偏移量))\n");
 		line.append("速度设置向导(S)\n");
 		line.append("重置Songs路径(R)\n");
+		line.append("键位设置(Y)\n");
 		for (size_t i = 0; i < buf.Height; i++) {
 			line.push_back('\n');
 		}
@@ -95,19 +172,19 @@ class SettingsScreen : public Screen {
 				game->Settings.Write();
 				return;
 			}
-			if (kea.UnicodeChar == '+' || kea.UnicodeChar == '-')
-			{
+			if (kea.Key == ConsoleKey::Y) {
+				parent->Navigate(new KeyBindingSetupScreen());
+				return;
+			}
+			if (kea.UnicodeChar == '+' || kea.UnicodeChar == '-') {
 				auto movement = 1.0;
-				if (HasFlag(kea.KeyState, ControlKeyState::Capslock))
-				{
+				if (HasFlag(kea.KeyState, ControlKeyState::Capslock)) {
 					movement = 10;
 				}
-				if (HasFlag(kea.KeyState,ControlKeyState::Shift))
-				{
+				if (HasFlag(kea.KeyState, ControlKeyState::Shift)) {
 					movement = 0.1;
 				}
-				if (kea.UnicodeChar == '-')
-				{
+				if (kea.UnicodeChar == '-') {
 					movement = -movement;
 				}
 				game->Settings["Offset"].Set(game->Settings["Offset"].Get<double>() + movement);
