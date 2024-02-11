50a29ce4a72ce2a750bd8efd9fe8d24448f2ffed
李家骏 <geometry2019@Outlook.com>
2024-02-11 21:50:59 +0800

    嘿嘿）

--- cmania/LinuxConsoleComponent.cpp
+++ cmania/LinuxConsoleComponent.cpp
@@ -13,6 +13,9 @@
 #include <thread>
 #include "signal.h"
 #include <unistd.h>
+#include <ranges>
+#include <codecvt>
+#include <locale>
 // class LinuxInput {
 // private:
 // 	static inline std::once_flag flag=std::once_flag();
@@ -36,6 +39,18 @@
 // };
 #define EVENT_POOL_SIZE 1024
 class InputProcessor {
+	class AnsiProcessor {
+	private:
+	public:
+		static inline std::size_t Process(char* c_in, std::size_t len, std::function<void(wchar_t t)> func) {
+			static std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+			static std::wstring ws = converter.from_bytes(std::string(c_in, len));
+			for (auto& t : ws) {
+				func(t);
+			}
+			return len;
+		}
+	};
 
 
 public:
@@ -57,76 +72,90 @@ public:
 		ONLY_DEVICE = 0b00000010,
 		BOTH = 0b00000100,
 	};
-	static std::size_t Process(char* c_in, int len, Event*& eventpool) {
-		char* c = buffer_enabled ? [&]() -> char* {
-			char* temp = new char[len + buffer_used_len];
-			memcpy(temp, buffer, buffer_used_len);
-			memcpy(temp + buffer_used_len, c_in, len);
-			return temp;
-		}()
-			: c_in;
-		int remain = len;
-		int event_pool_flag = 0;
-	loop:
-		// 如果是缓冲区已经处理完毕，那么delete掉
-		if (buffer_used_len == remain) {
-			delete[] c;
-			buffer_used_len = 0;
-			buffer_enabled = false;
-		}
-		if (remain != 1)
-			return 0;
-		if (remain >= 1) {
-			// 按照utf8的处理模式
-			int char_len = (c[0] & 0x80) == 0 ? 1 : (c[0] & 0xE0) == 0xC0 ? 2
-												: (c[0] & 0xF0) == 0xE0	  ? 3
-																		  : 4;
-			if (char_len == 0)
-				exit(0); // TODO:what the hell?
-			// 如果长度不够
-			if (remain < char_len)
-				goto fuck;
-			remain -= char_len;
-			// 如果是一个字符
-			if (char_len == 1) {
-				eventpool[event_pool_flag].type = EventType::Keyboard_Press;
-				memcpy(&eventpool[event_pool_flag].C, c, char_len);
-				event_pool_flag++;
-				// 如果还有剩余字符
-				if (remain) {
-					goto loop;
-				}
+
+	static void Process(char* c_in, int len, std::function<void(Event e)> func) {
+		// Process的返回值暂时不用了
+		auto f = [&](wchar_t t) {
+			// 如果是可打印字符
+			Event temp{};
+			if (t >= 32 && t <= 126) {
+				// eventpool[event_pool_flag].C = t;
+				// eventpool[event_pool_flag].type = EventType::Keyboard_Press;
+				temp.C = t;
+				temp.type = EventType::Keyboard_Press;
+				func(temp);
 			}
-			// 如果是一个字符
-		}
-		// 处理控制字符
-		if (c[0] == '\e') {
-			if (c[1] == '[') {
-				if (c[2] == 'A') {
-					eventpool[event_pool_flag].type = EventType::Mouse_Press;
-					eventpool[event_pool_flag].mouse_botton = c[3] - 32;
-					eventpool[event_pool_flag].x = c[4] - 32;
-					eventpool[event_pool_flag].y = c[5] - 32;
-					return event_pool_flag;
-				}
-			}
-		}
-	// 后面都是一无所获
-	fuck:
-		// 那么先放进缓冲区
-		if (buffer_enabled && buffer_used_len >= 4)
-			throw std::runtime_error("console解析出现问题了，部分字符不符合任意的utf8");
-		if (buffer_enabled) {
-			// 把新的复制到后面，然后len加上
-			memcpy(buffer + buffer_used_len, c + (len - remain), remain);
-			buffer_used_len += remain;
-			return 0;
-		}
-		memcpy(buffer, c + (len - remain), remain);
-		return 0;
+		};
+		AnsiProcessor::Process(c_in, len, f);
 	}
-	static void SetDeviceFileDescriptor(int fd) {
-	}
+	// static std::size_t Process(char* c_in, int len, Event*& eventpool) {
+	// 	char* c = buffer_enabled ? [&]() -> char* {
+	// 		char* temp = new char[len + buffer_used_len];
+	// 		memcpy(temp, buffer, buffer_used_len);
+	// 		memcpy(temp + buffer_used_len, c_in, len);
+	// 		return temp;
+	// 	}()
+	// 		: c_in;
+	// 	int remain = len;
+	// 	int event_pool_flag = 0;
+	// loop:
+	// 	// 如果是缓冲区已经处理完毕，那么delete掉
+	// 	if (buffer_used_len == remain) {
+	// 		delete[] c;
+	// 		buffer_used_len = 0;
+	// 		buffer_enabled = false;
+	// 	}
+	// 	if (remain >= 1) {
+	// 		// 按照utf8的处理模式
+	// int char_len = (c[0] & 0x80) == 0 ? 1 : (c[0] & 0xE0) == 0xC0 ? 2
+	// 									: (c[0] & 0xF0) == 0xE0	  ? 3
+	// 															  : 4;
+	// 		if (char_len == 0)
+	// 			exit(0); // TODO:what the hell?
+	// 		// 如果长度不够
+	// 		if (remain < char_len)
+	// 			goto fuck;
+	// 		remain -= char_len;
+	// 		// 如果是一个字符
+	// 		if (char_len == 1) {
+	// 			eventpool[event_pool_flag].type = EventType::Keyboard_Press;
+	// 			memcpy(&eventpool[event_pool_flag].C, c, char_len);
+	// 			event_pool_flag++;
+	// 			// 如果还有剩余字符
+	// 			if (remain) {
+	// 				goto loop;
+	// 			}
+	// 		}
+	// 		// 如果是一个字符
+	// 	}
+	// 	// 处理控制字符
+	// 	if (c[0] == '\e') {
+	// 		if (c[1] == '[') {
+	// 			if (c[2] == 'A') {
+	// 				eventpool[event_pool_flag].type = EventType::Mouse_Press;
+	// 				eventpool[event_pool_flag].mouse_botton = c[3] - 32;
+	// 				eventpool[event_pool_flag].x = c[4] - 32;
+	// 				eventpool[event_pool_flag].y = c[5] - 32;
+	// 				return event_pool_flag;
+	// 			}
+	// 		}
+	// 	}
+	// // 后面都是一无所获
+	// fuck:
+	// 	// 那么先放进缓冲区
+	// 	if (buffer_enabled && buffer_used_len >= 4)
+	// 		throw std::runtime_error("console解析出现问题了，部分字符不符合任意的utf8");
+	// 	if (buffer_enabled) {
+	// 		// 把新的复制到后面，然后len加上
+	// 		memcpy(buffer + buffer_used_len, c + (len - remain), remain);
+	// 		buffer_used_len += remain;
+	// 		return 0;
+	// 	}
+	// 	memcpy(buffer, c + (len - remain), remain);
+	// 	return 0;
+	// }
+	// static void SetDeviceFileDescriptor(int fd) {
+	// }
 	static void EnableFeature(Feature f) {
 		switch ((int)f) {
 		case (int)Feature::ONLY_ANSI:
@@ -144,12 +173,8 @@ public:
 
 private:
 	static inline int device = 0;
-	static inline std::array<Event, EVENT_POOL_SIZE> eventpool;
 	static inline std::once_flag flag = std::once_flag();
-	static inline std::size_t buffer_used_len = 0;
-	static inline char buffer[1024] = {};
 	static inline Feature feature = Feature::ONLY_ANSI;
-	static inline bool buffer_enabled = false;
 };
 class LinuxSignalHandler {
 public:
@@ -205,29 +230,31 @@ private:
 		}
 	}
 	static void InputWorker(Game* parent) {
-
-		//todo:这里读stdin,放到buffer
-		
-
 		char buf[1024];
-		int len = read(fileno(stdin), buf, 1024);
-		if (len > 0) {
-			InputProcessor::Event* e;
-			int i = InputProcessor::Process(buf, len, e);
-			if (i > 0) {
-				// 遍历每一个事件
-				for (int now = 0; now < i; now++) {
-					switch (e[now].type) {
+		while (1) {
+			int len = read(fileno(stdin), buf, 1024);
+			if (len > 0) {
+				InputProcessor::Event* e;
+				InputProcessor::Process(buf, len, [&](InputProcessor::Event ev) {
+					switch (ev.type) {
 					case InputProcessor::EventType::Mouse_Press:
 						// MouseKeyEventArgs meka(e[now].x, e[now].y, 1);
 						// parent->Raise("mousekey", meka);
 						// break;
-					case InputProcessor::EventType::Keyboard_Press:
-						KeyEventArgs kea(0, true, 0, e[now].C, 0);
+					case InputProcessor::EventType::Keyboard_Press: {
+						KeyEventArgs kea(0, true, 0, ev.C, 0);
+						if (isalpha(int(ev.C))) {
+							kea.Key = (ConsoleKey)(tolower(int(ev.C)) + 4);
+						}
 						parent->Raise("key", kea);
 						break;
 					}
-				}
+					default: {
+						break;
+					}
+					}
+				});
+				// 遍历每一个事件
 			}
 		}
 	}
